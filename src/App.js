bde1aa7ee88758e6f693fbcb74daaf77af55e1fb
tinaheiligers <tinaheiligers@gmail.com>
2018-07-01 15:36:38 -0700

    Adds placeholder for navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { push } from 'react-router-redux'
 import { history } from  './redux/store';
-import { Route, Switch, Link, Redirect } from 'react-router-dom';
 import { ConnectedRouter } from 'react-router-redux';
-import SignInBasic from './authComponents/signInBasic';
-import SignInWithGoogle from './authComponents/signInWithGoogle';
-import Home from './homeComponents/Home';
 import LoginOptions from './authComponents/loginOptions';
 
 class MainApp extends Component {
@@ -28,6 +23,7 @@ class Restricted extends Component {
     return(
       <ConnectedRouter history={history}>
         <div>
+          <p>Nav bar goes here</p>
           <h1>Restricted</h1>
           <button onClick={() => console.log('trigger logout action')}>Log Out</button>
         </div>
@@ -35,6 +31,7 @@ class Restricted extends Component {
     )
   }
 }
+
 class Public extends Component {
   render() {
     return(
@@ -47,85 +44,3 @@ class Public extends Component {
     )
   }
 }
-
-const App = props => {
-  const { history } = props;
-  return (
-    <ConnectedRouter history={history}>
-      <div>
-      <AuthButtonComponent />
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/protected">Protected Page</Link>
-          </li>
-        </ul>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <PrivateRoute path="/protected" component={Protected} />
-          <Route path="/login/options" component={LoginOptions} /> 
-          <Route path="/login/basic" component={SignInBasic} />
-          <Route path="/login/google" component={SignInWithGoogle} />
-        </Switch>
-        </div>
-    </ConnectedRouter>
-  );
-};
-
-//*******************************fake
-const fakeAuth = {
-  isAuthenticated: false,
-  authenticate(cb) {
-    this.isAuthenticated = true;
-    setTimeout(cb, 100); // fake async
-  },
-  signout(cb) {
-    this.isAuthenticated = false;
-    setTimeout(cb, 100);
-  }
-};
-//********************
-class AuthButton extends Component {
-  render() {
-    console.log(this.props)
-    const token = this.props.token;
-    if (token) {
-      return ( 
-        <p>Welcome!{" "}
-          <button onClick={this.props.signout}> Sign out </button>
-        </p>
-      );
-    } else {
-      return (
-        <p>You are not logged in.</p>
-      );
-    }
-  }
-}
-const AuthButtonComponent = connect(
-  state => ({
-    token: state.getIn(['auth', 'token']),
-  }), 
-  dispatch => {
-    { signout: () => { dispatch(push('/')) } }
-  })(AuthButton);
-
-//********************
-const PrivateRoute = ({ component: Component, ...rest }) => (
-  <Route
-    {...rest}
-    render={props => fakeAuth.isAuthenticated ? 
-      (<Component {...props} />) : (
-        <Redirect
-          to={{
-            pathname: "/login/options",
-            state: { from: props.location }
-          }}
-        />
-      )
-    }
-  />
-);
-const Protected = () => <h3>Protected</h3>;
